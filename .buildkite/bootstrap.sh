a4878f41e50bf04ef89dea598ff02f9530b76922
khluu <khluu000@gmail.com>
2026-02-24 01:48:21 -0800

    test
    Signed-off-by: khluu <khluu000@gmail.com>

--- .buildkite/bootstrap.sh
+++ .buildkite/bootstrap.sh
@@ -8,6 +8,7 @@ echo "Image: $IMAGE"
 echo "Config: $CONFIG"
 for config in $CONFIG; do
     if [[ $config == "h200_gpt_oss_120b" ]]; then IMAGE=$IMAGE buildkite-agent pipeline upload ./.buildkite/h200_gpt-oss-120b.yaml; fi
+    if [[ $config == "b200_gpt_oss_120b" ]]; then IMAGE=$IMAGE buildkite-agent pipeline upload ./.buildkite/b200_gpt-oss-120b.yaml; fi
     if [[ $config == "h200_qwen35_397b_a17b" ]]; then IMAGE=$IMAGE buildkite-agent pipeline upload ./.buildkite/h200_qwen35-397b-a17b.yaml; fi
     if [[ $config == "h200_qwen35_397b_a17b_fp8" ]]; then IMAGE=$IMAGE buildkite-agent pipeline upload ./.buildkite/h200_qwen35-397b-a17b-fp8.yaml; fi
 done
